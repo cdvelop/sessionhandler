c8dc3aaa3d4caa3ba2c299d5b94856751fd595d8
Cesar Solis <44058491+cdvelop@users.noreply.github.com>
2023-12-11 22:52:54 -0300

    add DecodeUser fun

--- build-user.go
+++ build-user.go
@@ -13,22 +13,27 @@ func (s Session) BuildUserFromStoreData(data []map[string]string) (u *model.User
 
 	if encode_user, ok := data[0][s.Session_encode]; ok && encode_user != "" {
 		// s.Log("encode_user:", encode_user)
-		session_decrypt, err := s.CipherAdapter.Decrypt(encode_user)
-		if err != "" {
-			return nil, this + err
-		}
-		// s.Log("Decrypt:", session_decrypt)
-
-		u = &model.User{}
-
-		err = s.DecodeStruct([]byte(session_decrypt), u)
-		if err != "" {
-			return nil, this + err
-		}
-
+		return s.DecodeUser(encode_user)
 		// s.Log("info USUARIO:", u.Name)
-
 	}
 
 	return
+}
+
+func (s Session) DecodeUser(encode_user string) (u *model.User, err string) {
+
+	session_decrypt, err := s.CipherAdapter.Decrypt(encode_user)
+	if err != "" {
+		return nil, err
+	}
+	// s.Log("Decrypt:", session_decrypt)
+
+	u = &model.User{}
+
+	err = s.DecodeStruct([]byte(session_decrypt), u)
+	if err != "" {
+		return nil, err
+	}
+
+	return u, ""
 }
