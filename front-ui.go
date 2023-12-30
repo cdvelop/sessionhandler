1133f2efabb5bbc6bcb0b39abb1a5761cbbdd52f
Cesar Solis <44058491+cdvelop@users.noreply.github.com>
2023-12-30 20:00:36 -0300

    auto update package

--- front-ui.go
+++ front-ui.go
@@ -5,10 +5,20 @@ import "github.com/cdvelop/model"
 func (s *Session) UserInterface(u *model.User) string {
 
 	c := &model.TemplateModuleConfig{
-		Module:      s.Object.Module,
-		Form:        s.Form,
-		AsideList:   nil,
-		ButtonLogin: true,
+		RenderAllSpaceCentered: true,
+		Module:                 s.Object.Module,
+		Form:                   s.Form,
+		FormButtons: []*model.ButtonForm{
+			{
+				ObjectName: s.Form.ObjectName,
+				ButtonName: "btn_" + s.Form.ObjectName,
+				Title:      s.Title,
+				IconID:     "icon-key",
+				OnclickFun: "submitLoginForm(this)",
+				Disabled:   true,
+			},
+		},
+		AsideList: nil,
 	}
 
 	return s.ModuleTemplate(c)
--- go.mod
+++ go.mod
@@ -3,15 +3,11 @@ module github.com/cdvelop/sessionhandler
 go 1.20
 
 require (
-	github.com/cdvelop/model v0.0.107
-	github.com/cdvelop/object v0.0.66
+	github.com/cdvelop/model v0.0.109
+	github.com/cdvelop/object v0.0.70
 )
 
 require (
 	github.com/cdvelop/strings v0.0.9 // indirect
 	github.com/cdvelop/structs v0.0.1 // indirect
 )
-
-replace github.com/cdvelop/model => ../model
-
-replace github.com/cdvelop/object => ../object
